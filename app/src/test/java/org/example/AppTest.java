2827b572b5b9f12604eaf5a2b94e1a17559fef78
masamasa0717 <masamasa0717@icloud.com>
2024-07-07 16:25:45 +0900

    Write test case

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -17,5 +17,4 @@ public class AppTest {
     App classUnderTest=new App();
     assertEquals("Hello World!", classUnderTest.getGreeting());
   }
-
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -9,4 +9,24 @@ public class CalculateTest {
     int expected = 5;
     assertEquals(expected, calculate.sum(2, 3));
   }
+
+  @test
+  public void testaverage(){
+    Calculate calculate2 = new Calculate();
+    double expected = 10;
+    assertEquals(expected, calculate.average(10, 30));
+  }
+  @test
+  public void testToSum(){
+    Calculate calculate2 = new Calculate();
+    double expected = 6;
+    assertEquals(expected, calculate.ToSum(1, 3));
+  }
+  @test
+  public void test ToSumAverage(){
+    Calculate calculate2 = new Calculate();
+    double expected = 3;
+    assertEquals(expected, calculate.ToSumAverage(1, 3));
+  }
+
 }
